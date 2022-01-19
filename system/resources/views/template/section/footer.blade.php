ebad8373769d61be44978132dc359cb9ec8e0a83
febriyani <90696202+fikrizulfikarb@users.noreply.github.com>
2022-01-19 18:59:17 +0700

    Initial commit

---
+++ system/resources/views/template/section/footer.blade.php
@@ -0,0 +1,7 @@
+
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2021  @if(date("Y") > '2021') - {{date("Y")}} @endif  <a href="https://fikrizulfikarb.blogspot.com/2021/11/is-my-blog-can-following-if-like-it.html">Fikri Zulfikar Brandan</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+      All rights reserved
+    </div>
+  </footer>
